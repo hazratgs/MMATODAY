a746eb24c52d4fd0e4ae5abe67cd532085375b24
hazratgs <hazratgs@gmail.com>
2018-05-02 18:53:52 +0300

    add styled-components

--- components/NewsCard/index.js
+++ components/NewsCard/index.js
@@ -1,137 +1,109 @@
 import React from 'react'
-import {
-  StyleSheet,
-  Text,
-  View,
-  Image
-} from 'react-native'
+import styled from 'styled-components'
+
+
+const CardView = styled.View`
+  background-color: #fff;
+  border-bottom-width: 8;
+  border-bottom-color: #eee;
+  padding-top: 15;
+  padding-bottom: 15;
+`
+const InfoView = styled.View`
+  margin-bottom: 8;
+  padding-left: 20;
+  padding-right: 20;
+  flex: 1;
+  flex-direction: row;
+  align-items: center
+`
+const Time = styled.Text`
+  font-size: 14;
+  color: #999;
+  margin-right: 10;
+`
+const Type = styled.Text`
+  border-width: 0.5;
+  border-color: rgba(56, 162, 76, 0.6);
+  border-radius: 1;
+  color: rgb(56, 162, 76);
+  font-size: 12;
+  font-weight: bold;
+  padding-left: 6;
+  padding-right: 6;
+  padding-top: 2;
+  padding-bottom: 2;
+  margin-right: 10;
+`
+const Author = styled.View`
+  flex-direction: row;
+  align-items: center;
+`
+const AuthorImg = styled.Image`
+  width: 20;
+  height: 20;
+  border-radius: 100;
+`
+const AuthorName = styled.Text`
+  font-size: 14;
+  color: #999;
+  margin-left: 10;
+`
+const Content = styled.View`
+  flex: 1;
+  padding-left: 20;
+  padding-right: ${props => props.type === 'big' ? 20 : 17};
+`
+const Title = styled.Text`
+  font-size: 18;
+  font-weight: 700;
+  line-height: 24;
+  margin-bottom: 6;
+`
+const Description = styled.Text`
+  font-size: 16;
+  line-height: 23;
+`
+const Images = styled.Image`
+  width: ${props => props.type === 'big' ? '100%' : '60'};
+  height: ${props => props.type === 'big' ? '280' : '60'};
+  margin-top: ${props => props.type === 'big' ? '20' : '5'};
+  border-radius: ${props => props.type === 'big' ? '0' : '1'};
+`
+const StyleContent = styled.View`
+  flex: 1;
+  display: flex;
+  flex-direction: ${props => props.type === 'big' ? 'column' : 'row'};
+  padding-right: ${props => props.type === 'big' ? '0' : '20'}
+`
+
 
 export default class NewsCard extends React.Component {
   render () {
     return (
-      <View style={s.card}>
-        <View style={s.info}>
-          <Text style={s.type}>{this.props.type}</Text>
-          <Text style={s.date}>{this.props.date}</Text>
-          <View style={s.author}>
-            <Image 
-              style={s.authorImg}
-              source={{uri: this.props.author.img}}
-            />
-            <Text style={s.authorName}>{this.props.author.name}</Text>
-          </View>
-        </View>
-        <View 
-          style={
-            this.props.style === 'big' 
-              ? s.big
-              : s.small
-          }
-        >
-          <View style={this.props.style === 'big' ? s.content : s.smallContent}>
-            <Text style={s.title}>{this.props.title}</Text>
-            <Text style={s.description}>
+      <CardView>
+        <InfoView>
+          <Type>{this.props.type}</Type>
+          <Time>{this.props.date}</Time>
+          <Author>
+            <AuthorImg source={{uri: this.props.author.img}}/>
+            <AuthorName>{this.props.author.name}</AuthorName>
+          </Author>
+        </InfoView>
+        <StyleContent type={this.props.style}>
+          <Content type={this.props.style}>
+            <Title>{this.props.title}</Title>
+            <Description>
               {this.props.description}
-            </Text>
-          </View>
-          <Image
-            style={this.props.style === 'big' ? s.img : s.smallImg}
+            </Description>
+          </Content>
+          <Images
+          type={this.props.style}
             source={{uri: this.props.img}}
             resizeMode='cover'
           />
-        </View>
-      </View>
+        </StyleContent>
+      </CardView>
     )
   }
 }
-
-const s = StyleSheet.create({
-  card: {
-    backgroundColor: '#fff',
-    borderBottomWidth: 8,
-    borderBottomColor: '#eee',
-    paddingTop: 15,
-    paddingBottom: 15
-  },
-  info: {
-    marginBottom: 8,
-    paddingLeft: 20,
-    paddingRight: 20,
-    flex: 1, 
-    flexDirection: 'row',
-    alignItems: 'center'
-  },
-  date: {
-    fontSize: 14,
-    color: '#999',
-    marginRight: 10
-  },
-  author: {
-    flexDirection: 'row',
-    alignItems: 'center',
-  },
-  authorImg: {
-    width: 20,
-    height: 20,
-    borderRadius: 100
-  },
-  authorName: {
-    fontSize: 14,
-    color: '#999',
-    marginLeft: 10
-  },
-  type: {
-    borderWidth: 0.5,
-    borderColor: 'rgba(56, 162, 76, 0.6)',
-    borderRadius: 1,
-    color: 'rgb(56, 162, 76)',
-    fontSize: 12,
-    fontWeight: 'bold',
-    paddingLeft: 6,
-    paddingRight: 6,
-    paddingTop: 2,
-    paddingBottom: 2,
-    marginRight: 10
-  },
-  content: {
-    flex: 1,
-    paddingLeft: 20,
-    paddingRight: 20
-  },
-  smallContent: {
-    flex: 1,
-    paddingLeft: 20,
-    paddingRight: 17
-  },
-  title: {
-    fontSize: 18,
-    fontWeight: '700',
-    lineHeight: 24,
-    marginBottom: 6,
-  },
-  description: {
-    fontSize: 16,
-    lineHeight: 23
-  },
-  img: {
-    flex: 1,
-    width: '100%',
-    height: 280,
-    marginTop: 20
-  },
-  big: {
-    flex: 1
-  },
-  small: {
-    display: 'flex',
-    flex: 1,
-    flexDirection: 'row',
-    paddingRight: 20
-  },
-  smallImg: {
-    width: 60,
-    height: 60,
-    borderRadius: 1,
-    marginTop: 5
-  }
-})
